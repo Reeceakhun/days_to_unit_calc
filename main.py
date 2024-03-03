81ac9b3a82d4bff38eaf52ba01e4fecb846d6ba0
bytewavetechnologies <info@bytewave-technologies.com>
2024-03-04 00:59:30 +0100

    nexted if

--- main.py
+++ main.py
@@ -19,10 +19,18 @@ def days_to_unit(days):
 def validate_and_execute ():
     if user_input.isdigit():
         user_input_number = int(user_input)
-        calculated_value = days_to_unit(user_input_number)
-        print(calculated_value)
+        if user_input_number > 0:
+            calculated_value = days_to_unit(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print ("You entered zero, please enter a valid positive number")
     else:
         print("You entered a non-digit value, don't ruin my program")
+
+
+user_input = input("Please enter the number of days\n")
+# convert the user input from string to integer
+validate_and_execute()
 
 
 user_input = input("Please enter the number of days\n")
